2dd6de7a450ba2de57ab5d530f0920f61805c63f
Morgan Fouesneau <morgan.fouesneau@astro.washington.edu>
2022-11-15 10:20:21 +0100

    tweak for example

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
+# Setup.py was never replaced by [PEP518](https://peps.python.org/pep-0518/)
+# and it is currently the only option to declare extension modules
 from setuptools import setup
 from distutils.core import Extension
 
 def get_extensions():
-    return [Extension(name='my_package.include.hello_world',
+    return [Extension(name='my_package.compiled.hello_world',
                       sources=['src/my_package/include/hello_world.pyx'])]
 
 setup(ext_modules=get_extensions())
