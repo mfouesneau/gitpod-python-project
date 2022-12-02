8c716c57bd2670967dc676bb1d04d61240c6181a
mfouesneau <fouesneau@mpia.de>
2022-12-02 09:53:46 +0000

    debugged cython extension

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 from distutils.core import Extension
 
 def get_extensions():
-    return [Extension(name='my_package.compiled.hello_world',
+    return [Extension(name='my_package.compiled_hello_world',
                       sources=['src/my_package/include/hello_world.pyx'])]
 
 setup(ext_modules=get_extensions())
